40988d194b81011b640b347e4973019aeffa63a0
Lean <leian1306@gmail.com>
2024-01-25 15:42:46 +0100

    Revert "app: configure state manager provider"
    This reverts commit ee76d40b70b91005a3a328b15496a9bbd2b66113.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-
-import StateProvider from './state-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,12 +20,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} min-w-[320px]`}>
-                <StateProvider>
-                    {children}
-                    <ReactQueryDevtools initialIsOpen={false} />
-                </StateProvider>
-            </body>
+            <body className={`${inter.className} min-w-[320px]`}>{children}</body>
         </html>
     );
 }
