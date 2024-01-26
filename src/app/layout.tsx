2f0a7cc1b6f94f2df0f33c29cd515c63e36d0959
Lean <leian1306@gmail.com>
2024-01-26 16:18:44 +0100

    app: configure next ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { Providers } from './providers';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -19,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={`${inter.className} min-w-[320px]`}>{children}</body>
+        <html lang="en" className="dark">
+            <body className={`${inter.className} min-w-[320px]`}>
+                <Providers>{children}</Providers>
+            </body>
         </html>
     );
 }
