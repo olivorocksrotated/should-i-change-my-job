6c9afa443a43505d32f854b2488ce5b9cf12edce
Lean <leian1306@gmail.com>
2024-02-04 20:42:16 +0100

    app: set up RxDB provider and initial collection

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,30 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
+import { addRxPlugin, RxDatabase } from 'rxdb';
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
+import { Provider as DatabaseProvider } from 'rxdb-hooks';
+
+import { initDatabase } from '@/lib/database/client';
+import { isDevEnvironment } from '@/lib/environments/is-dev';
+
+if (isDevEnvironment()) {
+    addRxPlugin(RxDBDevModePlugin);
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const [db, setDb] = useState<RxDatabase>();
+
+    useEffect(() => {
+        initDatabase().then(setDb);
+    }, []);
+
     return (
-        <NextUIProvider>{children}</NextUIProvider>
+        <DatabaseProvider db={db}>
+            <NextUIProvider>
+                {children}
+            </NextUIProvider>
+        </DatabaseProvider>
     );
 }
---
+++ src/lib/database/client.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+import { createRxDatabase } from 'rxdb';
+import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
+
+import { Collection } from './collections';
+import { storySchema } from './schemas';
+
+export const initDatabase = async () => {
+    try {
+        const db = await createRxDatabase({
+            name: 'should-i-quit-my-job-db',
+            storage: getRxStorageDexie()
+        });
+        console.info('Database created');
+
+        await db.addCollections({
+            [Collection.Stories]: { schema: storySchema }
+        });
+
+        return db;
+    } catch (error) {
+        console.error('Failed to create database', error);
+    }
+};
