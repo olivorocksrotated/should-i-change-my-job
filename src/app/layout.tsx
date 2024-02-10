7b996dffd612c9525e2d5020df3c2b4c15964265
Lean <leian1306@gmail.com>
2024-02-10 18:04:56 +0100

    layout: set the size to a mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className="dark">
-            <body className={`${inter.className} mx-auto min-h-screen min-w-[320px] max-w-3xl rounded border border-indigo-950 p-8`}>
+        <html lang="en" className="bg-gradient-to-r from-sky-500 to-indigo-500 p-6 dark">
+            <body className={`${inter.className} mx-auto h-[932px] w-[430px] max-w-3xl rounded border border-indigo-950 p-8`}>
                 <Providers>{children}</Providers>
             </body>
         </html>
