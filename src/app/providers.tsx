ea8d057cc1a84cb6f21635a0f943c849461d673c
Lean <leian1306@gmail.com>
2024-02-16 19:13:56 +0100

    Remove RxDb, it is not amazing to use
    Plus the docs are very questionable

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,31 +1,11 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
-import { useEffect, useState } from 'react';
-import { addRxPlugin, RxDatabase } from 'rxdb';
-import { disableWarnings, RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
-import { Provider as DatabaseProvider } from 'rxdb-hooks';
-
-import { initDatabase } from '@/lib/database/client';
-import { isDevEnvironment } from '@/lib/environments/is-dev';
-
-if (isDevEnvironment()) {
-    disableWarnings();
-    addRxPlugin(RxDBDevModePlugin);
-}
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    const [db, setDb] = useState<RxDatabase>();
-
-    useEffect(() => {
-        initDatabase().then(setDb);
-    }, []);
-
     return (
-        <DatabaseProvider db={db}>
-            <NextUIProvider>
-                {children}
-            </NextUIProvider>
-        </DatabaseProvider>
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
     );
 }
--- src/lib/database/client.ts
+++ src/lib/database/client.ts
@@ -1,25 +1,5 @@
-/* eslint-disable no-console */
-import { createRxDatabase } from 'rxdb';
-import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
-
-import { Collection } from './collections';
-import { storySchema } from './schemas';
-
 export const initDatabase = async () => {
     try {
-        const db = await createRxDatabase({
-            name: 'should-i-quit-my-job-db',
-            storage: getRxStorageDexie()
-        });
-        console.info('Database started');
-
-        if (!db[Collection.Stories]) {
-            await db.addCollections({
-                [Collection.Stories]: { schema: storySchema }
-            });
-        }
-
-        return db;
     } catch (error) {
         console.error('Failed to create database', error);
     }
