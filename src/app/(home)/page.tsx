0e7571ef78241bb0c256d3c02fe3f8b9a964299d
Lean <leian1306@gmail.com>
2024-02-17 17:58:02 +0100

    home: add choice cards

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { Button } from '@nextui-org/react';
+import { Card, CardBody, CardHeader } from '@nextui-org/react';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { AnimatePresence, motion } from 'framer-motion';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect } from 'react';
 
@@ -39,24 +40,90 @@ export default function HomePage() {
 
                     user?.showOnboarding ? <Onboarding user={user} /> : (
                         <article className="p-8">
-                            <section>
+                            <section className="mb-10">
                                 <h1 className="mb-2 text-4xl font-extralight">How quickly do you</h1>
                                 <h2 className="text-4xl">Want to make a decision?</h2>
                             </section>
 
-                            <section className="mb-4">
-                                <h2 className="mb-1">Very quickly, without much thought</h2>
-                                <Link href="/choices/random"><Button>Random choice</Button></Link>
+                            <section className="mb-12">
+                                <Link href="/choices/random">
+                                    <Card
+                                        className="w-full p-6"
+                                        classNames={{ base: 'overflow-visible', body: 'overflow-visible' }}
+                                        shadow="sm"
+                                        isPressable
+                                        isHoverable
+                                    >
+                                        <CardHeader className="mb-2 p-0 text-2xl font-extralight">
+                                            Fast and furious
+                                            <Image
+                                                className="absolute -right-6 -top-12"
+                                                priority={true}
+                                                src="/fast.svg"
+                                                alt="Fast decision"
+                                                width={150}
+                                                height={150}
+                                            />
+                                        </CardHeader>
+                                        <CardBody className="relative p-0">
+                                            <p className="text-sm">5 minutes</p>
+                                        </CardBody>
+                                    </Card>
+                                </Link>
                             </section>
 
-                            <section className="mb-4">
-                                <h2 className="mb-1">Somehow slowly, with some reflection</h2>
-                                <Link href="/choices/story"><Button>Story</Button></Link>
+                            <section className="mb-12">
+                                <Link href="/choices/story">
+                                    <Card
+                                        className="w-full p-6"
+                                        classNames={{ base: 'overflow-visible', body: 'overflow-visible' }}
+                                        shadow="sm"
+                                        isPressable
+                                        isHoverable
+                                    >
+                                        <CardHeader className="mb-2 p-0 text-2xl font-extralight">
+                                            Somehow slowly
+                                            <Image
+                                                className="absolute -right-6 -top-12"
+                                                priority={true}
+                                                src="/story.svg"
+                                                alt="Story decision"
+                                                width={150}
+                                                height={150}
+                                            />
+                                        </CardHeader>
+                                        <CardBody className="relative p-0">
+                                            <p className="text-sm">5 minutes</p>
+                                        </CardBody>
+                                    </Card>
+                                </Link>
                             </section>
 
-                            <section className="mb-4">
-                                <h2 className="mb-1">Slowly and safely</h2>
-                                <Link href="/choices/journal"><Button>Journal</Button></Link>
+                            <section>
+                                <Link href="/choices/journal">
+                                    <Card
+                                        className="w-full p-6"
+                                        classNames={{ base: 'overflow-visible', body: 'overflow-visible' }}
+                                        shadow="sm"
+                                        isPressable
+                                        isHoverable
+                                    >
+                                        <CardHeader className="mb-2 p-0 text-2xl font-extralight">
+                                             Slowly and safely
+                                            <Image
+                                                className="absolute -right-6 -top-8"
+                                                priority={true}
+                                                src="/slowly.svg"
+                                                alt="Slow decision"
+                                                width={150}
+                                                height={150}
+                                            />
+                                        </CardHeader>
+                                        <CardBody className="relative p-0">
+                                            <p className="text-sm">5 minutes</p>
+                                        </CardBody>
+                                    </Card>
+                                </Link>
                             </section>
                         </article>)}
             </AnimatePresence>
