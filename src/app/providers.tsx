8ca26fe9653bc39c0a27e7ad0ad2ccd19635a4e7
Lean <leian1306@gmail.com>
2024-03-22 18:11:07 +0100

    providers: remove unnecessary dexie initialization

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,14 +3,8 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { StoreInspector } from 'tinybase/ui-react-dom';
 
-import { createLocalUser } from '@/lib/database/create';
 import { dbName, Provider as DatabaseProvider } from '@/lib/database/store';
 import useInitStore from '@/lib/database/use-init-store';
-import { isClient, isIndexedDBAvailable } from '@/lib/environments/client';
-
-if (isClient() && isIndexedDBAvailable()) {
-    createLocalUser();
-}
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
